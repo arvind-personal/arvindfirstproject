ff8943030e82e2b9a1946a13800c821f7a12d854
arvind-personal <dev.arvind11@gmail.com>
2024-10-22 10:40:59 +0530

    Add files via upload

---
+++ OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.service.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI orderServiceConfig(){
+        return new OpenAPI()
+                .info(new Info().title("Order Service API")
+                        .description("Rest api for Order Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("you can refer Order service wiki documentation")
+                        .url("https://order_service_dummy_url.com/docs"));
+    }
+}
